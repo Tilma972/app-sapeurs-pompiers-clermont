f7895e5fc01a3e9dd861c4cf7f15b5389eba4f0a
steve_d <tilma972@gmail.com>
2025-10-29 14:14:24 +0100

    Layout: reduce vertical spacing in FocusedContainer for improved content alignment

--- components/layouts/focused/focused-container.tsx
+++ components/layouts/focused/focused-container.tsx
@@ -10,7 +10,7 @@ export function FocusedContainer({ children }: FocusedContainerProps) {
   return (
     <div className="bg-background">
       {/* Reduced vertical padding globally to bring page content closer to the AppBar */}
-      <div className="max-w-2xl mx-auto px-4 pt-0 pb-4 space-y-3">
+      <div className="max-w-2xl mx-auto px-4 pt-0 pb-4 space-y-2">
         {children}
       </div>
     </div>
