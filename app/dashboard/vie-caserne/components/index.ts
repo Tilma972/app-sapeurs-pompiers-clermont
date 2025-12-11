45b045799c0ea57d7a28b5b13b8532353dd1b0cf
steve_d <tilma972@gmail.com>
2025-12-11 14:32:43 +0100

    chore: remove dashboard folder and update revalidatePath calls

--- app/dashboard/vie-caserne/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { StatsCards } from './stats-cards'
-export { EventsTab } from './events-tab'
-export { MoneyPotsTab } from './money-pots-tab'
-export { MaterialsTab } from './materials-tab'
-export { PollsTab } from './polls-tab'
--- components/features/associative/index.ts
+++
@@ -1,9 +0,0 @@
-// ============================================
-// Composants UI - Module Vie de Caserne
-// ============================================
-
-export { CreateEventDialog } from './create-event-dialog'
-export { EventCard } from './event-card'
-export { MoneyPotCard } from './money-pot-card'
-export { MaterialCard } from './material-card'
-export { PollCard } from './poll-card'
